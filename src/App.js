760f32c0815cce6efb52f872c2368207c426f15f
bharathanhit <abharathan61@gmail.com>
2024-10-06 07:52:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import XO from './components/component/ox.js';
+
+function App() {
+  return (
+    <div className='app'>
+    <XO/>
+    </div>
+  );
+}
+
+export default App;
